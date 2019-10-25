02b1f0072bb62b260038a6dd27253069fddbbef0
Aden Herold <aden.herold1@gmail.com>
2019-10-25 13:32:56 +0930

    Implemented schema static to handle fetching a user by login credentials, and throwing an error upon failing to authenticate. Added new endpoint to routers/user.js to log a user in.

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -43,6 +44,23 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+//Hash password prior to saving
 userSchema.pre("save", async function(next) {
   const user = this;
 
