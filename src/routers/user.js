79f76ccbbdd34e33de007140925c52cae1fd4cf7
Aden Herold <aden.herold1@gmail.com>
2019-10-26 13:52:56 +0930

    Removed GET /users/:id endpoint from routers/users.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -66,32 +66,6 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
-router.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every(update =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates" });
-  }
-
-  try {
-    const user = await User.findById(req.params.id);
-    updates.forEach(update => (user[update] = req.body[update]));
-    await user.save();
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
 router.delete("/users/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
