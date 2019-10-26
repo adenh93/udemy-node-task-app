46ad3aae66191952c12631a1b8bac05156b96a25
Aden Herold <aden.herold1@gmail.com>
2019-10-26 15:55:12 +0930

    Added timestamps option to userSchema Schema and added new taskSchema Schema to models/task.js with timestamps option

--- src/models/task.js
+++ src/models/task.js
@@ -1,20 +1,27 @@
 const { model, Schema } = require("mongoose");
 
-const Task = model("Task", {
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+const taskSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
   },
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: true
   }
-});
+);
+
+const Task = model("Task", taskSchema);
 
 module.exports = Task;
--- src/models/user.js
+++ src/models/user.js
@@ -4,55 +4,60 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("../models/task");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!isEmail(value)) {
+          throw new Error("Email must be a properly formatted email address");
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password must not contain 'password'");
+        }
+        if (value.length < 6) {
+          throw new Error("Password must be at least 6 characters");
+        }
+      }
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number");
+        }
+      }
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!isEmail(value)) {
-        throw new Error("Email must be a properly formatted email address");
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password must not contain 'password'");
-      }
-      if (value.length < 6) {
-        throw new Error("Password must be at least 6 characters");
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    }
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true
-      }
-    }
-  ]
-});
+  {
+    timestamps: true
+  }
+);
 
 userSchema.virtual("tasks", {
   ref: "Task",
