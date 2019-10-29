35f61f3426058ce024822d2ae74cea8871a41e3e
Aden Herold <aden.herold1@gmail.com>
2019-10-29 12:29:40 +0930

    Added tests to tests/user.test.js to test that the GET /users/me and DELETE /users/me endpoints return the correct status code depending on whether or a suitable authorization header is provided

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,22 @@
 require("dotenv").config({ path: ".env.test" });
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = mongoose.Types.ObjectId();
+
 const userOne = {
+  _id: userOneId,
   name: "User One",
   email: "user.one@test.com",
-  password: "test123"
+  password: "test123",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }
+  ]
 };
 
 beforeEach(async () => {
@@ -43,4 +53,34 @@ test("Should not authenticate user with invalid credentials", async () => {
       password: "invalidPassword"
     })
     .expect(400);
+});
+
+test("Should get profile for authenticated user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile if user is not authenticated", async () => {
+  await request(app)
+    .get("/users/me")
+    .send()
+    .expect(401);
+});
+
+test("Should delete account for authenticated user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not delete account if not authenticated", async () => {
+  await request(app)
+    .delete("/users/me")
+    .send()
+    .expect(401);
 });
