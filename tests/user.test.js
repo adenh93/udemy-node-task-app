3f23d00637ab001c72e9c542ba6bc17496340bda
Aden Herold <aden.herold1@gmail.com>
2019-10-30 12:06:38 +0930

    Added test to user.test.js to test file uploads, using an image placed in a new fixtures directory. Added tests for the PATCH /users/me endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -59,7 +59,7 @@ test("Should login an existing user", async () => {
     })
     .expect(200);
 
-  const user = await User.findById(response.body.user._id);
+  const user = await User.findById(userOneId);
   expect(response.body.token).toBe(user.tokens[1].token);
 });
 
@@ -89,13 +89,13 @@ test("Should not get profile if user is not authenticated", async () => {
 });
 
 test("Should delete account for authenticated user", async () => {
-  const response = await request(app)
+  await request(app)
     .delete("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
 
-  const user = await User.findById(response.body._id);
+  const user = await User.findById(userOneId);
   expect(user).toBeNull();
 });
 
@@ -104,4 +104,38 @@ test("Should not delete account if not authenticated", async () => {
     .delete("/users/me")
     .send()
     .expect(401);
+});
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Another Test"
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual("Another Test");
+});
+
+test("Should not update invalid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: "Some Location"
+    })
+    .expect(400);
 });
