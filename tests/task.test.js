726422af27202157c9c2e1e19af803e1e69cda20
Aden Herold <aden.herold1@gmail.com>
2019-10-30 18:52:31 +0930

    Added extra test user and task data to fixtures/db.js, and added tests for GET /tasks and DELETE /tasksendpoints

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const Task = require("../src/models/task");
 const app = require("../src/app");
-const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
+const { userOne, userTwo, taskOne, setupDatabase } = require("./fixtures/db");
 
 beforeEach(setupDatabase);
 
@@ -17,4 +17,36 @@ test("Should create task for user", async () => {
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
   expect(task.completed).toBe(false);
+});
+
+test("Should return all tasks owned by user", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test("Should delete task belonging to user", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).toBeNull();
+});
+
+test("Should not delete task not belonging to user", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
 });
